15a06bf57f91f6ef4d82a9d6a4573fd22e8f8282
Marco <cerci.marco02@gmail.com>
2025-02-20 13:38:26 +0100

    “.”

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -1,3 +1,5 @@
+"Given an integer x, return true if x is a palindrome, and false otherwise."
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         y = str(x)
--- TwoSum.py
+++ TwoSum.py
@@ -1,3 +1,6 @@
+"""You may assume that each input would have EXACTLY ONE SOLUTION, and you may not use the same element twice.
+You can return the answer in any order."""
+
 from typing import List
 
 class Solution:
